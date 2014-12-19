743eb04f031dcb7da7801b62e534fd38f4832fcb
glisowski <lisux1991@tlen.pl>
2014-12-19 02:06:50 +0100

    Asset Clear

---
+++ Assets/Checkpoint.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Checkpoint : MonoBehaviour {
+    InZone[] playerInBounds;
+    Rigidbody player;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody>();
+        playerInBounds = GetComponentsInChildren<InZone>();
+    }
+
+    void Update()
+    {
+        if (playerInBounds[0] && !playerInBounds[1] && !playerInBounds[2] && !playerInBounds[3] && !playerInBounds[4] && !playerInBounds[5])
+        {
+            print(player.velocity.sqrMagnitude < 0.01f);
+        }
+    }
+}
