33bc497543e9adfd236439a7e2ffeec063501ac0
glisowski <lisux1991@tlen.pl>
2015-01-22 02:37:57 +0100

    repairs

---
+++ Assets/Scripts/Requirement.cs
@@ -0,0 +1,6 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class Requirement : MonoBehaviour {
+    public abstract bool passed();
+}
---
+++ Assets/Scripts/Stops.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Stops : Requirement {
+    Rigidbody player;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody>();
+    }
+
+    public override bool passed()
+    {
+        if (player.velocity.magnitude < 0.1)
+            return true;
+        else
+            return false;
+    }
+}
